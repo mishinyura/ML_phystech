58ec8a595c40b55a2c2baa4bed13ae5aca27c9bc
yuramishin <robothot@yandex.ru>
2024-10-29 21:52:53 +0300

    add funcs

--- library_numpy/main.py
+++ library_numpy/main.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 lst = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 ,19, 20]
+lst3 = [[1, 2],[3, 4]]
 
 print('array', np.array(lst))
 print('array*', np.array(lst) * 10)
@@ -22,4 +23,22 @@ print('row/column', lst[2,1])
 lst2 = 0 == (lst % 2)
 print('', lst2)
 
-print('', )
+left = np.arange(10).reshape(2, 5)
+right = np.arange(15).reshape(5, 3)
+print('left', left)
+print('right', right)
+print('dot', np.dot(left, right))
+
+rnd_lst = np.random.random((7, 5)) #Рандомная матрица
+print('random', rnd_lst)
+
+np.set_printoptions(4) #Установка знаков после запятой с округлением до целого (В большую и в меньшую)
+print('random', rnd_lst)
+
+print(lst[0,:])
+
+a = np.array([[1, 2, 3],
+             [4, 5, 6],
+             [7, 8, 9]])
+
+print(0 in a)
